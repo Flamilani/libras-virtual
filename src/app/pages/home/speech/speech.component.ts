ef53ceccbcef59e26edf06a8c72d144d4cd3abe9
Flavio Milani <milanidesign7@gmail.com>
2025-06-14 00:39:11 -0300

    ajustado transcricao audio

--- src/app/pages/home/speech/speech.component.ts
+++ src/app/pages/home/speech/speech.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-speech',
   templateUrl: './speech.component.html',
-  styleUrls: ['./speech.component.css']
+  styleUrls: ['./speech.component.css'],
 })
 export class SpeechComponent implements OnDestroy {
+  @ViewChild('textArea') textArea!: ElementRef;
+
   text = '';
   recognition: any;
   isListening = false;
+  fontSize: number = 20;
+  displayedValue!: string;
 
   constructor() {
     const SpeechRecognition =
@@ -37,7 +41,8 @@ export class SpeechComponent implements OnDestroy {
         this.isListening = true;
       };
 
-      this.recognition.onend = () => {
+      this.recognition.onend = (event: any) => {
+        console.error('Erro no reconhecimento:', event.error);
         this.isListening = false;
       };
     } else {
@@ -51,15 +56,51 @@ export class SpeechComponent implements OnDestroy {
     speechSynthesis.speak(utterance);
   }
 
+  toggleRecognition() {
+    if (this.isListening) {
+      this.stopRecognition();
+    } else {
+      this.startRecognition();
+    }
+  }
+
   startRecognition() {
-    if (this.recognition) {
+    if (this.recognition && !this.isListening) {
       this.recognition.start();
+      this.isListening = true;
+      this.focusTextArea();
     }
   }
 
   stopRecognition() {
-    if (this.recognition) {
+    if (this.recognition && this.isListening) {
       this.recognition.stop();
+      this.isListening = false;
+    }
+  }
+
+  focusTextArea() {
+    setTimeout(() => {
+      this.textArea?.nativeElement?.focus();
+    }, 0);
+  }
+
+  clearText() {
+    this.text = '';
+    this.focusTextArea();
+  }
+
+  increaseFontSize() {
+    if (this.fontSize < 160) {
+      this.fontSize += 20; // Aumenta o tamanho da fonte em 2 pixels
+    }
+  }
+
+  // Função para diminuir o tamanho da fonte
+  decreaseFontSize() {
+    if (this.fontSize > 80) {
+      // Limite mínimo de tamanho da fonte
+      this.fontSize -= 20; // Diminui o tamanho da fonte em 2 pixels
     }
   }
 
