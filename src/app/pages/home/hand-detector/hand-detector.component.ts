ca1eac7aa521f73b3606d774987709712a6b3ad9
Flavio Milani <milanidesign7@gmail.com>
2025-07-31 14:27:50 -0300

    🔖 Release 2.5.0

--- src/app/pages/home/hand-detector/hand-detector.component.ts
+++ src/app/pages/home/hand-detector/hand-detector.component.ts
@@ -7,14 +7,15 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { Camera } from '@mediapipe/camera_utils';
-import { Hands } from '@mediapipe/hands';
-import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
-import { HAND_CONNECTIONS } from '@mediapipe/hands';
 import { StringsNamesUrl } from 'src/app/shared/constants/strings-url/strings-names';
 import { CardUIComponent } from 'src/app/components/UI/card-ui/card-ui.component';
 import { BreadcrumbComponent } from 'src/app/shared/components/breadcrumb/breadcrumb.component';
 import { processString } from 'src/app/shared/utils/convert-urls';
+import {
+  GestureRecognizer,
+  FilesetResolver,
+  DrawingUtils,
+} from '@mediapipe/tasks-vision';
 
 @Component({
   selector: 'app-hand-detector',
@@ -23,293 +24,184 @@ import { processString } from 'src/app/shared/utils/convert-urls';
   templateUrl: './hand-detector.component.html',
   styleUrl: './hand-detector.component.css',
 })
-export class HandDetectorComponent implements OnInit, AfterViewInit, OnDestroy {
-   @ViewChild('video', { static: true }) videoRef!: ElementRef<HTMLVideoElement>;
-  @ViewChild('canvas', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
-
-  private camera!: Camera;
-  private mediaStream: MediaStream | null = null;
+export class HandDetectorComponent implements OnInit, OnDestroy {
+  @ViewChild('webcam', { static: true })
+  webcamRef!: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('output', { static: true })
+  gestureOutputRef!: ElementRef<HTMLParagraphElement>;
 
   title = StringsNamesUrl.detector;
   link = `/${processString(StringsNamesUrl.visorComputacional)}`;
 
-  detectedGesture: string = '';
+  private gestureRecognizer!: GestureRecognizer;
+  private runningMode: 'IMAGE' | 'VIDEO' = 'VIDEO';
+  private webcamRunning = false;
+  private lastVideoTime = -1;
+  private animationId: number | null = null;
 
-  private lastGestureDetected: string = '';
-  private animationFrameId: number | null = null;
-  private lastResults: any = null;
+  async ngOnInit(): Promise<void> {
+    await this.createGestureRecognizer();
+  }
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    const hands = new Hands({
-      locateFile: (file) =>
-        `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
-    });
-
-    hands.setOptions({
-      maxNumHands: 2,
-      modelComplexity: 1,
-      minDetectionConfidence: 0.7,
-      minTrackingConfidence: 0.7,
-    });
-
-    hands.onResults((results) => this.drawResults(results));
-
- /*    this.videoRef.nativeElement.onloadedmetadata = () => {
-      this.adjustCanvasSize();
-    }; */
-
-    this.camera = new Camera(this.videoRef.nativeElement, {
-      onFrame: async () => {
-        this.adjustCanvasSize();
-        await hands.send({ image: this.videoRef.nativeElement });
+  async createGestureRecognizer(): Promise<void> {
+    const vision = await FilesetResolver.forVisionTasks(
+      'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm'
+    );
+    this.gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath:
+          'https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task',
+        delegate: 'GPU',
       },
-      width: 640,
-      height: 480,
+      runningMode: this.runningMode,
     });
 
-    this.camera.start();
+    this.enableCam();
   }
 
-  scheduleDraw() {
-    if (this.animationFrameId !== null) {
-      cancelAnimationFrame(this.animationFrameId);
-    }
+  enableCam(): void {
+    const video = this.webcamRef.nativeElement;
 
-    this.animationFrameId = requestAnimationFrame(() => {
-      if (this.lastResults) {
-        this.drawResults(this.lastResults);
-      }
-    });
-  }
-
-  adjustCanvasSize(): void {
-    const canvas = this.canvasRef.nativeElement;
-    const video = this.videoRef.nativeElement;
-
-    const videoWidth = video.videoWidth;
-    const videoHeight = video.videoHeight;
-
-    if (!videoWidth || !videoHeight) return;
-
-    // Tamanho real do canvas (pixels)
-    canvas.width = videoWidth;
-    canvas.height = videoHeight;
-
-    // Tamanho visual do canvas (estilo)
-/*     const rect = video.getBoundingClientRect();
-
-    canvas.style.width = `${rect.width}px`;
-    canvas.style.height = `${rect.height}px`;
-    canvas.style.left = `${rect.left}px`;
-    canvas.style.top = `${rect.top}px`; */
-  }
-
-  drawResults(results: any) {
-    const canvasEl = this.canvasRef.nativeElement;
-    const canvasCtx = canvasEl.getContext('2d');
-    if (!canvasCtx) return;
-
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-
-    if (results.image) {
-      canvasCtx.drawImage(results.image, 0, 0, canvasEl.width, canvasEl.height);
-    }
-
-    if (results.multiHandLandmarks?.length) {
-      results.multiHandLandmarks.forEach((landmarks: any, index: any) => {
-        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
-          color: '#00FF00',
-          lineWidth: 2,
-        });
-        drawLandmarks(canvasCtx, landmarks, {
-          color: '#FF0000',
-          lineWidth: 2,
-          radius: 4,
-        });
-
-        // 🟨 Debug visual com rótulos por dedo
-        /*        const drawLabel = (text: string, x: number, y: number) => {
-          canvasCtx.font = '12px Arial';
-          canvasCtx.fillStyle = 'gray';
-          canvasCtx.fillText(text, x, y);
-        };
-
-        const debugPoints = [
-          { name: 'Polegar', index: 4 },
-          { name: 'Indicador', index: 8 },
-          { name: 'Médio', index: 12 },
-          { name: 'Anelar', index: 16 },
-          { name: 'Mindinho', index: 20 },
-        ];
-
-        debugPoints.forEach((point) => {
-          const x = landmarks[point.index].x * canvasEl.width;
-          const y = landmarks[point.index].y * canvasEl.height;
-          const label = `${point.name}: x=${landmarks[point.index].x.toFixed(
-            2
-          )} y=${landmarks[point.index].y.toFixed(2)}`;
-          drawLabel(label, x + 5, y - 5);
-        });
- */
-        const handLabel = results.multiHandedness?.[index]?.label; // 'Left' or 'Right'
-        const gesture = this.detectGesture(landmarks, handLabel);
-
-        if (gesture && gesture !== this.lastGestureDetected) {
-          this.detectedGesture = gesture;
-          this.lastGestureDetected = gesture;
-          this.speak(gesture);
-        }
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      video.srcObject = stream;
+      video.addEventListener('loadeddata', () => {
+        this.adjustCanvasSize();
+        this.predictWebcam();
       });
-    } else {
-      // Quando a mão some da câmera
-      this.detectedGesture = '';
-      this.lastGestureDetected = '';
+    });
+  }
+
+  async predictWebcam(): Promise<void> {
+    const video = this.webcamRef.nativeElement;
+    const canvas = this.canvasRef.nativeElement;
+    const canvasCtx = canvas.getContext('2d')!;
+    const gestureOutput = this.gestureOutputRef.nativeElement;
+
+    if (this.runningMode === 'IMAGE') {
+      this.runningMode = 'VIDEO';
+      await this.gestureRecognizer.setOptions({ runningMode: 'VIDEO' });
     }
 
-    canvasCtx.restore();
-  }
+    const nowInMs = Date.now();
+    if (video.currentTime !== this.lastVideoTime) {
+      this.lastVideoTime = video.currentTime;
+      const results = await this.gestureRecognizer.recognizeForVideo(
+        video,
+        nowInMs
+      );
 
-  detectGesture(landmarks: any, handLabel: string): string | null {
-    if (this.detectJoinha(landmarks) && handLabel === 'Right')
-      return '👍 Joinha com a mão direita';
-    if (this.detectJoinha(landmarks) && handLabel === 'Left')
-      return '👍 Joinha com a mão esquerda';
-    if (this.detectJoinhaInvertido(landmarks) && handLabel === 'Right')
-      return '👎 Não Curtir com a direita';
-    if (this.detectJoinhaInvertido(landmarks) && handLabel === 'Left')
-      return '👎 Não Curtir com a esquerda';
-    if (this.detectLetraL(landmarks) && handLabel === 'Left')
-      return 'Letra L com a esquerda';
-    if (this.detectLetraL(landmarks) && handLabel === 'Right')
-      return 'Letra L com a direita';
-    return null;
-  }
+      canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // === GESTURES ===
+      const drawingUtils = new DrawingUtils(canvasCtx);
 
-  detectJoinha(landmarks: any): boolean {
-    return (
-      this.areFingersFolded(landmarks) &&
-      landmarks[4].y < landmarks[3].y &&
-      landmarks[4].y < landmarks[0].y &&
-      Math.abs(landmarks[4].x - landmarks[0].x) < 0.15
-    );
-  }
+      if (results.landmarks) {
+        for (const landmarks of results.landmarks) {
+          drawingUtils.drawConnectors(
+            landmarks,
+            GestureRecognizer.HAND_CONNECTIONS,
+            { color: '#00FF00', lineWidth: 5 }
+          );
+          drawingUtils.drawLandmarks(landmarks, {
+            color: '#FF0000',
+            lineWidth: 2,
+          });
+        }
+      }
 
-  detectJoinhaInvertido(landmarks: any): boolean {
-    return (
-      this.areFingersFolded(landmarks) &&
-      landmarks[4].y > landmarks[3].y &&
-      landmarks[4].y > landmarks[0].y &&
-      Math.abs(landmarks[4].x - landmarks[0].x) < 0.15
-    );
-  }
+      const gestureTranslations: { [key: string]: string } = {
+        Open_Palm: 'Mão Aberta',
+        Closed_Fist: 'Punho Fechado',
+        Pointing_Up: 'Apontando para Cima',
+        Thumb_Up: 'Polegar para Cima',
+        Thumb_Down: 'Polegar para Baixo',
+        Victory: 'Vitória',
+        ILoveYou: 'Eu Amo Você',
+      };
 
-  detectLetraA(landmarks: any): boolean {
-    return this.areFingersFolded(landmarks) && landmarks[4].x < landmarks[3].x;
-  }
+      const handednessTranslations: { [key: string]: string } = {
+        Left: 'Esquerda',
+        Right: 'Direita',
+      };
 
-  detectLetraB(landmarks: any): boolean {
-    return (
-      this.areFingersExtended(landmarks) &&
-      landmarks[4].y > landmarks[3].y &&
-      Math.abs(landmarks[4].x - landmarks[2].x) < 0.06
-    );
-  }
+      if (results.gestures.length > 0) {
+        gestureOutput.style.display = 'block';
+        const categoryName = results.gestures[0][0].categoryName;
+        //  const categoryScore = (results.gestures[0][0].score * 100).toFixed(2);
+        const handedness = results.handednesses[0][0].displayName;
 
-  detectLetraL(landmarks: any): boolean {
-    const indicadorParaCima =
-      landmarks[8].y < landmarks[6].y && landmarks[6].y < landmarks[5].y;
+        const translatedGesture =
+          gestureTranslations[categoryName] || categoryName;
+        const translatedHand = handednessTranslations[handedness] || handedness;
 
-    const polegarAfastado =
-      landmarks[4].x > landmarks[2].x + 0.05 && // afastamento lateral
-      landmarks[4].y > landmarks[3].y; // mais abaixo do indicador
+        let cssClass = '';
+        let emoji = '';
 
-    const outrosDobrados =
-      this.isFingerFolded(12, 10, landmarks) &&
-      this.isFingerFolded(16, 14, landmarks) &&
-      this.isFingerFolded(20, 18, landmarks);
+        switch (categoryName) {
+          case 'Open_Palm':
+            cssClass = 'gesture-open';
+            emoji = '🖐️';
+            break;
+          case 'Closed_Fist':
+            cssClass = 'gesture-fist';
+            emoji = '✊';
+            break;
+          case 'Thumb_Up':
+            cssClass = 'gesture-thumb-up';
+            emoji = '👍';
+            break;
+          case 'Thumb_Down':
+            cssClass = 'gesture-thumb-down';
+            emoji = '👎';
+            break;
+          case 'Victory':
+            cssClass = 'gesture-victory';
+            emoji = '✌️';
+            break;
+          case 'ILoveYou':
+            cssClass = 'gesture-iloveyou';
+            emoji = '🤟';
+            break;
+          case 'Pointing_Up':
+            cssClass = 'gesture-pointing';
+            emoji = '☝️';
+            break;
+          default:
+            cssClass = '';
+            emoji = '🖐️';
+        }
 
-    return indicadorParaCima && polegarAfastado && outrosDobrados;
-  }
+        gestureOutput.className = `gesture-output ${cssClass}`
+        gestureOutput.style.display = 'block';
+        gestureOutput.innerText = `${emoji} Sinal: ${translatedGesture} | Lado: ${translatedHand}`;
+      } else {
+        gestureOutput.style.display = 'none';
+      }
+    }
 
-  detectLetraS(landmarks: any): boolean {
-    return this.areFingersFolded(landmarks) && landmarks[4].x > landmarks[2].x;
-  }
-
-  detectOi(landmarks: any): boolean {
-    return (
-      this.areFingersExtended(landmarks) &&
-      this.isFingerExtended(4, 3, landmarks)
-    );
-  }
-
-  detectAjuda(landmarks: any): boolean {
-    return (
-      this.isFingerExtended(8, 6, landmarks) &&
-      this.isFingerExtended(4, 3, landmarks) &&
-      this.isFingerFolded(12, 10, landmarks) &&
-      this.isFingerFolded(16, 14, landmarks) &&
-      this.isFingerFolded(20, 18, landmarks)
-    );
-  }
-
-  // === UTILS ===
-
-  isFingerFolded(tip: number, base: number, landmarks: any): boolean {
-    const dx = landmarks[tip].x - landmarks[base].x;
-    const dy = landmarks[tip].y - landmarks[base].y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    return distance < 0.1;
-  }
-
-  isFingerExtended(tip: number, base: number, landmarks: any): boolean {
-    const dx = landmarks[tip].x - landmarks[base].x;
-    const dy = landmarks[tip].y - landmarks[base].y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    return distance > 0.15;
-  }
-
-  areFingersFolded(landmarks: any): boolean {
-    return (
-      this.isFingerFolded(8, 5, landmarks) &&
-      this.isFingerFolded(12, 9, landmarks) &&
-      this.isFingerFolded(16, 13, landmarks) &&
-      this.isFingerFolded(20, 17, landmarks)
-    );
-  }
-
-  areFingersExtended(landmarks: any): boolean {
-    return (
-      this.isFingerExtended(8, 5, landmarks) &&
-      this.isFingerExtended(12, 9, landmarks) &&
-      this.isFingerExtended(16, 13, landmarks) &&
-      this.isFingerExtended(20, 17, landmarks)
-    );
-  }
-
-  speak(text: string) {
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = 'pt-BR';
-    window.speechSynthesis.cancel();
-    window.speechSynthesis.speak(utterance);
+    this.animationId = requestAnimationFrame(() => this.predictWebcam());
   }
 
   ngOnDestroy(): void {
-    if (this.mediaStream) {
-      this.mediaStream.getTracks().forEach((track) => track.stop());
-    }
-
-    if (this.camera) {
-      this.camera.stop();
+    if (this.animationId) cancelAnimationFrame(this.animationId);
+    const video = this.webcamRef.nativeElement;
+    const stream = video.srcObject as MediaStream;
+    if (stream) {
+      stream.getTracks().forEach((track) => track.stop());
     }
   }
 
-  /*   @HostListener('window:resize')
+  adjustCanvasSize(): void {
+    const video = this.webcamRef.nativeElement;
+    const canvas = this.canvasRef.nativeElement;
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+  }
+
+  @HostListener('window:resize')
   onResize() {
     this.adjustCanvasSize();
-  } */
+  }
 }
